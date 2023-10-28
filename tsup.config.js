6365b285cb5cf1db064c3da398fa9e7073b250ac
Alexandre Mogère <heristop@yahoo.fr>
2023-10-28 21:17:32 +0200

    [Build] Used tsup for package

---
+++ tsup.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  external: ["react"],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    };
+  },
+  loader: {
+    ".svg": "dataurl",
+  },
+});
