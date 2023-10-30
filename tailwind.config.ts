dc22e539783173a69bf00dafae7816bdb386e2a6
Alexandre Mogère <heristop@yahoo.fr>
2023-10-30 10:22:53 +0100

    [Tailwind] Up config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "**/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/linkfolio/dist/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
