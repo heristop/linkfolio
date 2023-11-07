52513c15705c9d5a35788cc6539db33eb1e87385
Alexandre Mogère <heristop@yahoo.fr>
2023-11-07 23:09:12 +0100

    [Package] Upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,27 +23,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+        <meta
+          name="application-name"
+          content={userConfig.metaTitle ?? "LinkFolio"}
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content={userConfig.metaTitle ?? "LinkFolio"}
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
+        <meta
+          name="description"
+          content={userConfig.metaDescription ?? "LinkFolio"}
         />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta
+          name="msapplication-TileColor"
+          content={userConfig.themeColor ?? "#2f5d62"}
+        />
         <meta name="theme-color" content={userConfig.themeColor ?? "#2f5d62"} />
         <meta property="og:type" content="website" />
         <meta
@@ -71,6 +73,31 @@ export default function RootLayout({
         <meta
           property="twitter:description"
           content={userConfig.metaDescription ?? "LinkFolio"}
+        />
+
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color={userConfig.themeColor ?? "#2f5d62"}
         />
 
         <Analytics />
